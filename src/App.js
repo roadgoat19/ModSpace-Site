f0cf137aa1db6acbba58675a0f7d7dc90339346e
roadgoat19 <kenneth.hoskins@gmail.com>
2023-10-03 22:23:10 -0400

    commiting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const [modulos, setModulos] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Counter">
+        <button 
+          onClick={() => modulos !== 0 ? setCounter((prevCount) => (prevCount - 1 + modulos) % modulos) : setCounter(() => 0)}>
+          -
+        </button>
+        <h1>{counter}</h1>
+        <button 
+          onClick={() => modulos !== 0 ? setCounter((prevCount) => (prevCount + 1) % modulos) : setCounter(() => 0)}>
+          +
+        </button>
+      </div>
+      <div className="ModSpace">
+        <button onClick={() => setModulos((prevModulos) => Math.max(0, prevModulos - 1))}>-</button>
+        <h1>Mod: {modulos}</h1>
+        <button onClick={() => setModulos((prevModulos) => prevModulos + 1)}>+</button>
+      </div>
     </div>
   );
 }
